ae082f6c7f8e28bd694eab8434ea32df1ea033cd
tbauer516 <tcbauer516@gmail.com>
2016-10-02 11:52:19 -0700

    setup node for test portfolio page

---
+++ server.js
@@ -0,0 +1,78 @@
+var   http         = require('http'),
+      express      = require('express'),
+      fs           = require('fs'),
+      path         = require('path'),
+      contentTypes = require('./utils/content-types'),
+      sysInfo      = require('./utils/sys-info'),
+      env          = process.env;
+
+// var server = http.createServer(function (req, res) {
+//   var url = req.url;
+//   if (url == '/') {
+//     url += 'index.html';
+//   }
+
+//   // IMPORTANT: Your application HAS to respond to GET /health with status 200
+//   //            for OpenShift health monitoring
+
+//   if (url == '/health') {
+//     res.writeHead(200);
+//     res.end();
+//   } else if (url == '/info/gen' || url == '/info/poll') {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.setHeader('Cache-Control', 'no-cache, no-store');
+//     res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+//   } else {
+//     fs.readFile('./static' + url, function (err, data) {
+//       if (err) {
+//         res.writeHead(404);
+//         res.end('Not found');
+//       } else {
+//         let ext = path.extname(url).slice(1);
+//         res.setHeader('Content-Type', contentTypes[ext]);
+//         if (ext === 'html') {
+//           res.setHeader('Cache-Control', 'no-cache, no-store');
+//         }
+//         res.end(data);
+//       }
+//     });
+//   }
+// });
+
+var app = express();
+
+// app.get('/', function(req, res) {
+// 	var url = req.url;
+// 	if (url == '/health') {
+// 		res.writeHead(200);
+// 		res.end();
+// 	} else if (url == '/info/gen' || url == '/info/poll') {
+// 		res.setHeader('Content-Type', 'application/json');
+// 		res.setHeader('Cache-Control', 'no-cache, no-store');
+// 		res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+// 	} else {
+// 		fs.readFile('./public' + url, function (err, data) {
+// 			if (err) {
+// 				res.writeHead(404);
+// 				res.end('Not found');
+// 			} else {
+// 				var ext = path.extname(url).slice(1);
+// 				res.setHeader('Content-Type', contentTypes[ext]);
+// 				if (ext === 'html') {
+// 					res.setHeader('Cache-Control', 'no-cache, no-store');
+// 				}
+// 				res.end(data);
+// 			}
+// 		});
+// 	}
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+// server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+//   console.log(`Application worker ${process.pid} started...`);
+// });
+
+app.listen(env.NODE_PORT || 3000, function() {
+  console.log(`Application worker ${process.pid} started on port ` + (env.NODE_PORT || 3000) + `...`);
+});
