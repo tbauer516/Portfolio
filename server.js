bd88599ca3b323efcdc88ee29df95cd380f94ee6
tbauer516 <tcbauer516@gmail.com>
2016-10-02 14:13:35 -0700

    add gzip

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var compression = require('compression');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+var oneDay = 86400000;
+
+app.use(compression());
+
+app.use(express.static(__dirname + '/public', {maxAge: oneDay}));
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 var ipadress = process.env.OPENSHIFT_NODEJS_IP;
